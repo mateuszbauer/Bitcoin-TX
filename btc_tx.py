ad648d109827c2cbff95c69c5cd51530c7b458de
mateuszbauer <bauermateusz@gmail.com>
2023-05-24 15:58:20 +0200

    cleanup

--- btc_tx.py
+++ btc_tx.py
@@ -7,6 +7,7 @@ import os, struct, time
 import socket
 
 VERSION = 70002
+MAGIC = 0xD9B4BEF9
 
 def dsha256(x):
 	return hashlib.sha256(hashlib.sha256(x).digest()).digest()
@@ -14,142 +15,116 @@ def dsha256(x):
 def sha256(x):
 	return hashlib.sha256(x).digest()
 
-'''
-Solution for "ValueError: unsupported hash type ripemd160":
-	https://github.com/openssl/openssl/issues/16994
-'''
 def ripemd160(x):
+	'''
+	Solution for "ValueError: unsupported hash type ripemd160":
+		https://github.com/openssl/openssl/issues/16994
+	'''
 	h = hashlib.new('ripemd160')
 	h.update(x)
 	return h.digest()
 
-'''
-From https://developer.bitcoin.org/devguide/wallets.html#private-key-formats
+def btc_to_satoshi(x):
+	return int(x * 1000000000)
 
-In Bitcoin, a private key in standard format is 256-bit number, between the values:
-0x01 and 0xFFFF FFFF FFFF FFFF FFFF FFFF FFFF FFFE BAAE DCE6 AF48 A03B BFD2 5E8C D036 4140
-'''
 def create_private_key():
+	'''
+	https://developer.bitcoin.org/devguide/wallets.html#private-key-formats
+	'''
 	return os.urandom(32)
 
 def create_public_key(private_key):
 	pk = ecdsa.SigningKey.from_string(private_key,
 		curve=ecdsa.SECP256k1).get_verifying_key().to_string('compressed')
-
 	assert (len(pk) == 33)
 	assert (pk[0] == 0x02 or pk[0] == 0x03)
 	return pk
 
-'''
-From https://developer.bitcoin.org/devguide/wallets.html#wallet-import-format-wif
-
-Assuming mainnet address and compressed public key:
-1. Take a private key
-2. Add a 0x80 byte in front of it for mainnet addresses or 0xef for testnet addresses
-3. Append a 0x01 byte after it if it should be used with compressed public keys 
-4. Perform a SHA-256 hash on the extended key.
-5. Perform a SHA-256 hash on result of SHA-256 hash.
-6. Take the first four bytes of the second SHA-256 hash; this is the checksum.
-7. Add the four checksum bytes from point 5 at the end of the extended key from point 2.
-8. Convert the result from a byte string into a Base58 string using Base58Check encoding.
-'''
 def create_wallet_import_format(private_key):
+	'''
+	https://developer.bitcoin.org/devguide/wallets.html#wallet-import-format-wif
+	'''
 	extended_key = b'\x80' + private_key + b'\x01'
 	checksum = dsha256(extended_key)[:4]
 	return base58.b58encode((extended_key + checksum))
 	
-'''
-From https://en.bitcoin.it/wiki/Protocol_documentation#Addresses
-'''
 def generate_address(public_key):
-	version = b'\x00'
-	key_hash = version + ripemd160(sha256(public_key))
+	'''
+	https://en.bitcoin.it/wiki/Protocol_documentation#Addresses
+	'''
+	key_hash = b'\x00' + ripemd160(sha256(public_key))
 	checksum = dsha256(key_hash)[:4]
-	address = base58.b58encode(key_hash + checksum)
-	return address
+	return base58.b58encode(key_hash + checksum)
 
 def generate_qrcode(address, file):
 	qrcode.make(address).save(file)
 
-'''
-From https://en.bitcoin.it/wiki/Protocol_documentation#Common_structures
-
------------+-------------+-----------+----------------------------------------------------
-Field size | Description | Data type | Comment
------------+-------------+-----------+----------------------------------------------------
-4          | magic       | uint32_t  | 0xD9B4BEF9 for main network, 0xDAB5BFFA for testnet
------------+-------------+-----------+----------------------------------------------------
-12         | command     | char[12]  | NULL padded, ascii string identifying content 
------------+-------------+-----------+----------------------------------------------------
-4          | length      | uint32_t  | Payload length in number of bytes
------------+-------------+-----------+----------------------------------------------------
-4          | checksum    | uint32_t  | First 4 bytes of sha256(sha256(payload))
------------+-------------+-----------+----------------------------------------------------
-?          | payload     | uchar[]   | Actual data
------------+-------------+-----------+----------------------------------------------------
-'''
 def pack_message(command, payload):
-	magic = 0xD9B4BEF9
-	return struct.pack('<I12sI4s', magic, command.encode('ascii'),
+	'''
+	From https://en.bitcoin.it/wiki/Protocol_documentation#Common_structures
+	'''
+	return struct.pack('<I12sI4s', MAGIC, command.encode('ascii'),
 		len(payload), dsha256(payload)[:4]) + payload
 
 def recv_message(sock):
 	magic, command, length, checksum = struct.unpack('<I12sI4s', sock.recv(24))
 	payload = sock.recv(length)
-	assert magic == 0xD9B4BEF9
+	assert magic == MAGIC
 	assert length == len(payload)
 	assert checksum == dsha256(payload)[:4]
 	return command.decode('ascii'), payload
 
 def prepare_version_msg():
-	version = VERSION   # 4 bytes
-	services = 1 # 8 bytes
-	timestamp = int(time.time()) # 8 bytes
-	addr_recv = b'\x00' * 26 # 26 bytes
-	addr_from = b'\x00' * 26 # 26 bytes
-	nonce = os.urandom(8) # 8 bytes
-	user_agent = b'\x00' # 0x00 if 0 bytes long
-	start_height = b'\x00' # 4 bytes
-
-	payload = struct.pack('<IQQ26s26s8s1s4s', version, services, timestamp,
+	'''
+	https://en.bitcoin.it/wiki/Protocol_documentation#version
+	'''
+	services = 1
+	addr_recv = b'\x00' * 26
+	addr_from = b'\x00' * 26
+	nonce = os.urandom(8)
+	user_agent = b'\x00'
+	start_height = b'\x00\x00\x00\x00'
+	payload = struct.pack('<IQQ26s26s8s1s4s', VERSION, services, int(time.time()),
 		addr_recv, addr_from, nonce, user_agent, start_height)
+
 	return pack_message('version', payload)
 
-def btc_to_satoshi(x):
-	return int(x * 1000000000)
-
 def prepare_verack_msg():
+	'''
+	https://en.bitcoin.it/wiki/Protocol_documentation#verack
+	'''
 	return pack_message('verack', b'')
 
-def sign_tx_msg(raw_tx, private_key):
-	pass
-
 def prepare_raw_tx(tx_hash_id, value, recv_addr):
-	# TODO: rewrite cause this looks bad
+	'''
+	https://en.bitcoin.it/wiki/Protocol_documentation#tx
+	'''
 	version = 1
 	tx_in_count = 1
 	tx_hash_id = binascii.unhexlify(tx_hash_id)
+	assert len(tx_hash_id) == 32
 	output_index = 0
 	sequence = b'\xff\xff\xff\xff'
 	tx_out_count = 1
 	value = btc_to_satoshi(value)
 	lock_time = b'\x00\x00\x00\x00'
-
 	scriptPubKey = b'\x76' + b'\xa9' + b'\x14' + base58.b58decode(recv_addr) + b'\x88' + b'\xac'
 	scriptSig = scriptPubKey
-
-	return struct.pack('<IB', version, tx_in_count) + tx_hash_id + \
-		struct.pack('<IB', output_index, len(scriptSig)) + scriptSig + \
-		sequence + struct.pack('<BQB', tx_out_count, value, len(scriptPubKey)) + \
+	return struct.pack('<IB32sIB', version, tx_in_count, tx_hash_id, output_index, len(scriptSig)) + \
+		scriptSig + struct.pack('<4sBQB', sequence, tx_out_count, value, len(scriptPubKey)) + \
 		scriptPubKey + lock_time
+
+def sign_tx_msg(raw_tx, private_key):
+	pass
 
 def prepare_tx_msg(tx_hash_id, value, private_key, recv_addr):
 	raw_tx = prepare_raw_tx(tx_hash_id, value, recv_addr)
 	signed_tx = sign_tx_msg(raw_tx, private_key)
+	return signed_tx
 
 def main():
-	sample_hash = "eccf7e3034189b851985d871f91384b8ee357cd47c3024736e5676eb2debb3f2"
 	value = 0.9999
+	sample_hash = "81b4c832d70cb56ff957589752eb4125a4cab78a25a8fc52d6a09e5bd4404d48"
 	addr = "1KZ51E55Q7z4DANd74mj4xu4rnBfW5FyzY"
 	raw_tx = prepare_raw_tx(sample_hash, value, addr)
 	hexdump(raw_tx)
@@ -180,7 +155,6 @@ def main():
 	print(cmd)
 	hexdump(payload)
 	'''
-
 
 if __name__ == '__main__':
 	main()
